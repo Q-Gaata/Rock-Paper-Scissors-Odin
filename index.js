4da7ff2830219f74491435de4a8e623ec9396f0c
Q-Gaata <alabigata@gmail.com>
2022-10-24 09:48:41 +0100

    Play one round of the game

--- index.js
+++ index.js
@@ -11,5 +11,43 @@ function getComputerChoice(){
     return randomArr
 }
 
+function playOneRound(playerSelection, computerSelection){
+    // Declare a result variable
+    let result = '';
+    // Play game to check who wins
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'rock'){
+            result = `It's a tie!`
+        } else if(computerSelection === 'scissors'){
+            playerScore++
+            result = `You win! Rock beats Scissors.`
+        } else if(computerSelection === 'paper') {
+            result = `You lose! Paper beats Rock`
+            computerScore++
+        }
+    } else if(playerSelection === 'paper'){
+        if(computerSelection === 'rock'){
+            result = `You win! Paper beats Rock`
+            playerScore++
+        } else if(computerSelection === 'scissors'){
+            result = `You lose! Scissors beats Paper`
+            computerScore++
+        } else if(computerSelection === 'paper') {
+            result = `It's a tie`
+        }
+    } else if(playerSelection === 'scissors'){
+        if(computerSelection === 'scissors'){
+            result = `It's a tie`
+        } else if(computerSelection === 'paper'){
+            result = `You win! Scissors beats Paper`
+            playerScore++
+        } else if(computerSelection === 'rock') {
+            result = `You lose! Rock beats Scissors`
+            computerScore++
+        }
+    }
+
+    return result
+}
 
 

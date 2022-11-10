1c46d63e5060d34c2d2f22edd96e191a84b22dee
Q-Gaata <alabigata@gmail.com>
2022-11-10 11:22:19 +0100

    Add User Interface

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const buttons = document.getElementsByTagName('button')
+const playerButtons = document.querySelector('.players-button')
+const play = document.querySelector('.play')
+const body = document.body
+
+console.log(body)
+
+let player = document.querySelector('#ply')
+let computer = document.querySelector('#cpt')
+let newResult = document.querySelector('.result')
 let playerScore = 0;
 let computerScore = 0;
+
 
 function getComputerChoice(){
     // Create an array of three elements
@@ -11,63 +22,183 @@ function getComputerChoice(){
     return randomArr
 }
 
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function (){
+    const computerSelection = getComputerChoice()
+    playOneRound(buttons[i].textContent.toLowerCase(), computerSelection)
+    })
+}
+
 function playOneRound(playerSelection, computerSelection){
     // Declare a result variable
     let result = '';
+    
     // Play game to check who wins
     if(playerSelection === 'rock'){
         if(computerSelection === 'rock'){
-            result = `It's a tie! Score: ${playerScore} to ${computerScore}`
+            result = `It's a tie!`
         } else if(computerSelection === 'scissors'){
             playerScore++
-            result = `You win! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`
+            result = `You win! Rock beats Scissors.`
         } else if(computerSelection === 'paper') {
             computerScore++
-            result = `You lose! Paper beats Rock. Score: ${playerScore} to ${computerScore}`
+            result = `You lose! Paper beats Rock`
         }
 
     } else if(playerSelection === 'paper'){
         if(computerSelection === 'rock'){
             playerScore++
-            result = `You win! Paper beats Rock. Score: ${playerScore} to ${computerScore}`
+            result = `You win! Paper beats Rock.`
             
         } else if(computerSelection === 'scissors'){
             computerScore++
-            result = `You lose! Scissors beats Paper. Score: ${playerScore} to ${computerScore}`
+            result = `You lose! Scissors beats Paper.`
         } else if(computerSelection === 'paper') {
-            result = `It's a tie. Score: ${playerScore} to ${computerScore}`
+            result = `It's a tie.`
         }
 
     } else if(playerSelection === 'scissors'){
         if(computerSelection === 'scissors'){
-            result = `It's a tie. Score: ${playerScore} to ${computerScore}`
+            result = `It's a tie.`
         } else if(computerSelection === 'paper'){
             playerScore++
-            result = `You win! Scissors beats Paper. Score: ${playerScore} to ${computerScore}`
+            result = `You win! Scissors beats Paper.`
             
         } else if(computerSelection === 'rock') {
             computerScore++
-            result = `You lose! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`
+            result = `You lose! Rock beats Scissors.`
         }
     }
 
-    return result
+    newResult.textContent = result;
+    newResult.classList.add('trans')
+    player.textContent = `Player: ${playerScore}`;
+    computer.textContent = `Computer: ${computerScore}`;
+
+    stopGame()
+   
 }
 
-function game(){
+function stopGame(){
+    if(playerScore == 5){
+        newResult.textContent = `Congratulations! You win`
+        playerButtons.innerHTML = ''
+        play.style.display = 'block'
+        body.style.backgroundColor = '#4FA095'
+    } else if(computerScore == 5){
+        newResult.textContent = `Game Over!!!`
+        playerButtons.innerHTML = ''
+        play.style.display = 'block'
+        body.style.backgroundColor = '#E0144C'
+    } 
     
-    for(i = 0; i < 5; i++){
-        const playerSelection = prompt('Choose among Rock, Paper or Scissors').toLowerCase()
-        const computerSelection = getComputerChoice()
-        console.log(playOneRound(playerSelection,computerSelection))
-    }
-    
-    if(playerScore > computerScore){
-        console.log(`Congratulations! You win by ${playerScore} to ${computerScore}`)
-    } else if(computerScore > playerScore){
-        console.log(`Game over!! You lose by ${playerScore} to ${computerScore}`)
-    } else {
-        console.log(`Draw! Play again`)
-    }
 }
-game()
+
+function playAgain(){
+    play.addEventListener('click', () => {
+        player.textContent = 'Player:'
+        computer.textContent = 'Computer:'
+        location.reload()
+        
+    })
+
+}
+playAgain()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Function Constructor
+// const Person = function(name, yearOfBirth, school, gender){
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.school = school;
+//     this.gender = gender;
+//   }
+  
+//   Person.prototype.calculateAge = function(){
+//     console.log(2016 - this.yearOfBirth)
+//   }
+//   Person.prototype.getNameGender = function(){
+//     console.log(`My name is ${this.name} and I study at ${this.school} and I am a ${this.gender}`)
+//   }
+
+//   var john = new Person('John', 2002, 'Unilag', 'Male')
+//   var kamal = new Person('Kamal', 1998, 'Unilorin', 'Male')
+//   var abike = new Person('Abike', 2000, 'University of Oxford', 'Female')
+
+//   john.calculateAge()
+//   kamal.getNameGender()
+//   abike.getNameGender()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game(){
+    
+//     for(i = 0; i < 5; i++){
+//         const playerSelection = prompt('Choose among Rock, Paper or Scissors').toLowerCase()
+//         const computerSelection = getComputerChoice()
+//         console.log(playOneRound(playerSelection,computerSelection))
+//     }
+    
+//     if(playerScore > computerScore){
+//         console.log(`Congratulations! You win by ${playerScore} to ${computerScore}`)
+//     } else if(computerScore > playerScore){
+//         console.log(`Game over!! You lose by ${playerScore} to ${computerScore}`)
+//     } else {
+//         console.log(`Draw! Play again`)
+//     }
+// }
+// game()
+
